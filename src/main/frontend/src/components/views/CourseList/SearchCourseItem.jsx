2e69cb9845145b961591d90575231a6483ea97b2
JoGeumJu <ccho8702@naver.com>
2022-10-07 00:36:36 +0900

    SearchCoursePage db연동 Initial Tool

--- src/main/frontend/src/components/views/CourseList/SearchCourseItem.jsx
+++ src/main/frontend/src/components/views/CourseList/SearchCourseItem.jsx
@@ -1,44 +1,42 @@
-import React,{useState,useEffect} from 'react';
-import styled from 'styled-components';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
-const Tr = styled.tr`
-`;
+const Tr = styled.tr``;
 
 const Td = styled.td`
-    border:0px;
-    padding: 0.3vw;
-    margin:0px;
-    font-size: 0.8rem;
-    background-color: #FFFFFF;
-    border-bottom:1px solid gray;
+  border: 0px;
+  padding: 0.3vw;
+  margin: 0px;
+  font-size: 0.8rem;
+  background-color: #ffffff;
+  border-bottom: 1px solid gray;
 `;
 
-function SearchCourseItem(props){
+function SearchCourseItem(props) {
+  // const [items, setItems] = useState([]);
 
-    // const [items, setItems] = useState([]);
+  // useEffect(()=>{
+  //     axios.get('/api/courses').them((res)=>{
+  //         setItems(res.data)
+  //         console.log(items);
+  //     });
+  // }, []);
 
-    // useEffect(()=>{
-    //     axios.get('/api/courses').them((res)=>{
-    //         setItems(res.data)
-    //         console.log(items);
-    //     });
-    // }, []);
-
-
-    return(
-        <Tr>
-            <Td name={props.indexNumber}></Td>
-            <Td name={props.courseDepartment}></Td>
-            <Td name={props.courseGrade}></Td>
-            <Td name={props.courseName}></Td>
-            <Td name={props.courseNumber}></Td>
-            <Td name={props.courseCredit}></Td>
-            <Td name={props.courseMaxNumber}></Td>
-            <Td name={props.courseRequest}></Td>
-            <Td name={props.courseTime}></Td> */}
-            <Td name={props.courseProfessor}></Td>
-            {/* <Td name={props.courseSortation}></Td>
+  return (
+    <Tr>
+      <Td name={props.id}></Td>
+      <Td name={props.department}></Td>
+      <Td name={props.grade}></Td>
+      <Td name={props.divison}></Td>
+      <Td name={props.subject_id}></Td>
+      <Td name={props.subject_type}></Td>
+      <Td name={props.grades}></Td>
+      <Td name={props.max_count}></Td>
+      <Td name={props.register_count}></Td>
+      <Td name={props.subject_time}></Td>
+      <Td name={props.professor}></Td>
+      {/* <Td name={props.courseSortation}></Td>
             <Td name={props.courseClassification}></Td>
             <Td name={props.courseDistribution}></Td>
             <Td name={props.coursePreRequest}></Td>
@@ -46,8 +44,8 @@ function SearchCourseItem(props){
             <Td name={props.coursePractice}></Td>
             <Td name={props.courseArea}></Td>
             <Td name={props.courseNote}></Td> */}
-        </Tr>
-    );
+    </Tr>
+  );
 }
 
 export default SearchCourseItem;
--- src/main/frontend/src/components/views/CourseList/SearchCourseList.jsx
+++ src/main/frontend/src/components/views/CourseList/SearchCourseList.jsx
@@ -1,93 +1,101 @@
-import React,{} from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
 
-import SearchCourseItem from './SearchCourseItem';
+import SearchCourseItem from "./SearchCourseItem";
 
 const Wrapper = styled.div`
-    width: 100%;
-    height:100%;
-    overflow:auto;
-    box-sizing: border-box;
-    margin: 0px;
-    padding: 0px;
-    overflow-y: auto;
-    overflow-x: visible;
-    &::-webkit-scrollbar{
-        width: 10px;
-        height: 5px;
-        boder-radius: 10px;
-    }
-    &::-webkit-scrollbar-track{
-        background:rgba(255,255,255,0,0);
-    }
-    &::-webkit-scrollbar-thumb{
-        background:rgba(0,0,0,0.2);
-        background-clip:padding-box;
-        border: 2px solid transparent;
-        border-radius: 100px;
-    }
-    &::-webkit-scrollbar-thumb:hover{
-        background:rgba(0,0,0,0.3);
-        background-clip:padding-box;
-        border: 1.5px solid transparent;
-        border-radius: 100px;
-    }
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+  overflow-y: auto;
+  overflow-x: visible;
+  &::-webkit-scrollbar {
+    width: 10px;
+    height: 5px;
+    boder-radius: 10px;
+  }
+  &::-webkit-scrollbar-track {
+    background: rgba(255, 255, 255, 0, 0);
+  }
+  &::-webkit-scrollbar-thumb {
+    background: rgba(0, 0, 0, 0.2);
+    background-clip: padding-box;
+    border: 2px solid transparent;
+    border-radius: 100px;
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background: rgba(0, 0, 0, 0.3);
+    background-clip: padding-box;
+    border: 1.5px solid transparent;
+    border-radius: 100px;
+  }
 `;
 
 const Table = styled.table`
-    width:100%;
-    background-color: white;
-    margin:0px;
-    padding:0px;
-    border-spacing:0px;
-    border-style:none;
+  width: 100%;
+  background-color: white;
+  margin: 0px;
+  padding: 0px;
+  border-spacing: 0px;
+  border-style: none;
 `;
 
 const Tr = styled.tr`
-    width:100%;
+  width: 100%;
 `;
 
 const Th = styled.th`
-    padding: 0.45vw;
-    padding-bottom:0.23vw;
-    margin:0px;
-    font-size: 0.9rem;
-    background-color: #FFCC1D;
-    color: #ffffff;
-    border:0px;
-    position: sticky;
-	top: 0;
+  padding: 0.45vw;
+  padding-bottom: 0.23vw;
+  margin: 0px;
+  font-size: 0.9rem;
+  background-color: #ffcc1d;
+  color: #ffffff;
+  border: 0px;
+  position: sticky;
+  top: 0;
 `;
 
-function SearchCourseList(props){
+function SearchCourseList(props) {
+  const courseListsNum = axios.get("/api/test");
 
-    return (
-        <Wrapper>
-            <Table>
-                <Tr>
-                    <Th name="indexNumber" style={{borderTopLeftRadius:'15px'}}>No.</Th>
-                    <Th name="courseDepartment">개설학과</Th>
-                    <Th name="courseGrade">학년</Th>
-                    <Th name="courseName">과목이름</Th>
-                    <Th name="courseNumber">과목코드</Th>
-                    <Th name="courseClassification">이수구분</Th>
-                    <Th name="courseCredit">학점</Th>
-                    <Th name="courseMaxNumber">정원</Th>
-                    <Th name="courseRequest">수강신청</Th>
-                    <Th name="courseTime">수업시간</Th>
-                    <Th name="courseProfessor" style={{borderTopRightRadius:'15px'}}>담당교수</Th>
-                    {/*<Th name="courseSortation">수업구분</Th>
+  return (
+    <Wrapper>
+      <Table>
+        <Tr>
+          <Th name="id" style={{ borderTopLeftRadius: "15px" }}>
+            No.
+          </Th>
+          <Th name="department">개설학과</Th>
+          <Th name="grade">학년</Th>
+          <Th name="divison">과목이름</Th>
+          <Th name="subject_id">과목코드</Th>
+          <Th name="subject_type">이수구분</Th>
+          <Th name="grades">학점</Th>
+          <Th name="max_count">정원</Th>
+          <Th name="register_count">신청인원</Th>
+          <Th name="subject_time">수업시간</Th>
+          <Th name="professor" style={{ borderTopRightRadius: "15px" }}>
+            담당교수
+          </Th>
+          {/*<Th name="courseSortation">수업구분</Th>
                     <Th name="coursePreRequest">예비신청(비율)</Th>--------------------
                     <Th name="courseTheory">이론</Th>
                     <Th name="coursePractice">실습</Th>
                     <Th name="courseArea">영역</Th>
                     <Th name="courseNote" style={{borderTopRightRadius:'15px'}}>비고</Th> */}
-                    {/* <Th name="courseDistribution">분반</Th> */}
-                </Tr>
-                <SearchCourseItem/>
-            </Table>
-        </Wrapper>
-    );
+          {/* <Th name="courseDistribution">분반</Th> */}
+        </Tr>
+        {courseListsNum.data.content.map((index) => (
+          <SearchCourseItem courseList={index} />
+        ))}
+      </Table>
+    </Wrapper>
+  );
 }
 
 export default SearchCourseList;
