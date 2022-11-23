480eaffed00196cfbc14874a31989ab1fdbda288
DESKTOP-UATT97O\kkjj4 <gmpin12@gmail.com>
2022-11-23 20:34:27 +0900

    수정

--- src/main/frontend/src/components/views/SidebarPage/MiniTable.jsx
+++ src/main/frontend/src/components/views/SidebarPage/MiniTable.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import styled from "styled-components";
 import { useRecoilValue } from "recoil";
@@ -8,131 +7,113 @@ import TimeTableClass from "./MiniTableClass";
 // ===== 시간표 컴포넌트 =====
 
 function MiniTable(props) {
-    const registeredClass = useRecoilValue(userRegisterState);
+  const registeredClass = useRecoilValue(userRegisterState);
 
-    return (
-        <Wrapper>
-            <TableOuter>
-                <TableWrapper>
-                    <Table>
-                        <colgroup>
-                            <col width="20%" />
-                            <col width="16%" />
-                            <col width="16%" />
-                            <col width="16%" />
-                            <col width="16%" />
-                            <col width="16%" />
-                        </colgroup>
-                        <thead>
-                        <Tr>
-                            <Th>시간</Th>
-                            <TdDay>월</TdDay>
-                            <TdDay>화</TdDay>
-                            <TdDay>수</TdDay>
-                            <TdDay>목</TdDay>
-                            <TdDay>금</TdDay>
-                        </Tr>
-                        </thead>
-                        <tbody>
-                        <Tr>
-                            <Th>
-                                1교시
-                            </Th>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                        </Tr>
-                        <Tr>
-                            <Th>
-                                2교시
-                            </Th>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                        </Tr>
-                        <Tr>
-                            <Th>
-                                3교시
-                            </Th>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                        </Tr>
-                        <Tr>
-                            <Th>
-                                4교시
-                            </Th>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                        </Tr>
-                        <Tr>
-                            <Th>
-                                5교시
-                            </Th>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                        </Tr>
-                        <Tr>
-                            <Th>
-                                6교시
-                            </Th>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                        </Tr>
-                        <Tr>
-                            <Th>
-                                7교시
-                            </Th>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                        </Tr>
-                        <Tr>
-                            <Th>
-                                8교시
-                            </Th>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                        </Tr>
-                        <Tr>
-                            <Th>
-                                9교시
-                            </Th>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                            <Td></Td>
-                        </Tr>
-                        </tbody>
-                    </Table>
-                    {registeredClass.map((item) => {
-                        return <TimeTableClass item={item} />;
-                    })}
-                </TableWrapper>
-            </TableOuter>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <TableOuter>
+        <TableWrapper>
+          <Table>
+            <colgroup>
+              <col width="20%" />
+              <col width="16%" />
+              <col width="16%" />
+              <col width="16%" />
+              <col width="16%" />
+              <col width="16%" />
+            </colgroup>
+            <thead>
+              <Tr>
+                <Th>시간</Th>
+                <TdDay>월</TdDay>
+                <TdDay>화</TdDay>
+                <TdDay>수</TdDay>
+                <TdDay>목</TdDay>
+                <TdDay>금</TdDay>
+              </Tr>
+            </thead>
+            <tbody>
+              <Tr>
+                <Th>1교시</Th>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+              <Tr>
+                <Th>2교시</Th>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+              <Tr>
+                <Th>3교시</Th>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+              <Tr>
+                <Th>4교시</Th>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+              <Tr>
+                <Th>5교시</Th>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+              <Tr>
+                <Th>6교시</Th>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+              <Tr>
+                <Th>7교시</Th>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+              <Tr>
+                <Th>8교시</Th>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+              <Tr>
+                <Th>9교시</Th>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+                <Td></Td>
+              </Tr>
+            </tbody>
+          </Table>
+          {registeredClass.map((item) => {
+            return <TimeTableClass item={item} />;
+          })}
+        </TableWrapper>
+      </TableOuter>
+    </Wrapper>
+  );
 }
 
 export default MiniTable;
@@ -157,34 +138,33 @@ const Table = styled.table`
   text-align: center;
   background-color: #ffcc1d;
   color: #404040;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
   overflow: hidden;
   table-layout: fixed;
-  font-size:13px;
+  font-size: 12px;
 `;
 const Tr = styled.tr`
   padding: 0;
   margin: 0;
-  border-bottom: 2px solid #ffca1d3b;
+  /* border-bottom: 2px solid #ffca1d3b; */
+  height: 23px;
 `;
 const TdDay = styled.td`
   padding: 0;
   background-color: #ffcc1d;
-  border-left: solid 1px white;
-  border-bottom: solid 1px white;
   white-space: nowrap;
+  padding-top: 3px;
 `;
 const Th = styled.td`
   background-color: #ffcc1d;
-  border-bottom: solid 1px white;
+  // border-bottom: solid 1px white;
   overflow: hidden;
+  padding-top: 3px;
 `;
 const Td = styled.td`
   padding: 0;
-  border-left: solid 1px white;
+  /* border-left: solid 1px white; */
   background-color: #fffdad;
   color: #000000;
 `;
-
-
